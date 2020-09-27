52717c65c9f6c120aa05f305a61dbf5a5bb2798b
Zakhar Vorsulev <vorsulev@gmail.com>
2020-09-27 16:56:26 -0400

    script edit

--- script.js
+++ script.js
@@ -4,60 +4,59 @@ let operation = null
 const inputWindow = document.getElementById('inputWindow');
 const inputWindow2 = document.getElementById('inputWindow2');
 
-
 document.getElementById('btn_1').addEventListener('click', function () {
     inputWindow.value += '1'
-    inputWindow2.value += '1'
+    addToHistoty('1')
 })
 
 document.getElementById('btn_2').addEventListener('click', function () {
     inputWindow.value += '2'
-    inputWindow2.value += '2'  
+    addToHistoty('2')  
 }) 
 
 document.getElementById('btn_3').addEventListener('click', function () {
     inputWindow.value += '3' 
-    inputWindow2.value += '3' 
+    addToHistoty('3') 
 })
 
 document.getElementById('btn_4').addEventListener('click', function () {
     inputWindow.value += '4'  
-    inputWindow2.value += '4'
+    addToHistoty('4')
 })
 
 document.getElementById('btn_5').addEventListener('click', function () {
     inputWindow.value += '5'  
-    inputWindow2.value += '5'
+    addToHistoty('5')
 })
 
 document.getElementById('btn_6').addEventListener('click', function () {
     inputWindow.value += '6' 
-    inputWindow2.value += '6' 
+    addToHistoty('6') 
 })
 
 document.getElementById('btn_7').addEventListener('click', function () {
     inputWindow.value += '7'  
-    inputWindow2.value += '7'
+    addToHistoty('7')
 })
 
 document.getElementById('btn_8').addEventListener('click', function () {
     inputWindow.value += '8'  
-    inputWindow2.value += '8'
+    addToHistoty('8')
 })
 
 document.getElementById('btn_9').addEventListener('click', function () {
     inputWindow.value += '9'  
-    inputWindow2.value += '9'
+    addToHistoty('9')
 })
 
 document.getElementById('btn_0').addEventListener('click', function () {
     inputWindow.value += '0' 
-    inputWindow2.value += '0' 
+    addToHistoty('0') 
 })
 
 document.getElementById('btn_dot').addEventListener('click', function () {
     inputWindow.value += '.'
-    inputWindow2.value += '.'
+    addToHistoty('.')
     lastOperand = parseFloat(inputWindow.value)  
 })
 
@@ -65,7 +64,7 @@ document.getElementById('btn_sum').addEventListener('click', function () {
     lastOperand = parseFloat(inputWindow.value)
     operation = 'sum'
     inputWindow.value = ''
-    inputWindow2.value += '+'
+    addToHistoty('+')
     
 })
 
@@ -73,21 +72,21 @@ document.getElementById('btn_sub').addEventListener('click', function () {
     lastOperand = parseFloat(inputWindow.value)
     operation = 'sub'
     inputWindow.value = ''
-    inputWindow2.value += '-'
+    addToHistoty('-')
 })
 
 document.getElementById('btn_mult').addEventListener('click', function () {
     lastOperand = parseFloat(inputWindow.value)
     operation = 'mult'
     inputWindow.value = ''
-    inputWindow2.value += '*'
+    addToHistoty('*')
 })
 
 document.getElementById('btn_division').addEventListener('click', function () {
     lastOperand = parseFloat(inputWindow.value)
     operation = 'division'
     inputWindow.value = ''
-    inputWindow2.value += '÷'
+    addToHistoty('÷')
 })
 
 document.getElementById('btn_root').addEventListener('click', function () {
@@ -95,7 +94,7 @@ document.getElementById('btn_root').addEventListener('click', function () {
     operation = null
     const result =  Math.sqrt(inputWindow.value)
     inputWindow.value = result
-    inputWindow2.value += '√=' + result 
+    addToHistoty('√=' + result) 
 })
 
 document.getElementById('btn_notion').addEventListener('click', function () {
@@ -112,7 +111,7 @@ document.getElementById('btn_calc').addEventListener('click', function () {
          operation = null
          lastOperand = 0
          inputWindow.value = result
-         inputWindow2.value += '=' + result + ' '
+         addToHistoty('=' + result + ' ')
      }
 
      if (operation === 'sub'){
@@ -120,7 +119,7 @@ document.getElementById('btn_calc').addEventListener('click', function () {
         operation = null
         lastOperand = 0
         inputWindow.value = result
-        inputWindow2.value += '=' + result + ' '
+        addToHistoty('=' + result + ' ')
     }
 
      if (operation === 'mult'){
@@ -128,7 +127,7 @@ document.getElementById('btn_calc').addEventListener('click', function () {
         operation = null
         lastOperand = 0
         inputWindow.value = result
-        inputWindow2.value += '=' + result + ' '
+        addToHistoty('=' + result + ' ')
     }
 
      if (operation === 'division'){
@@ -136,7 +135,7 @@ document.getElementById('btn_calc').addEventListener('click', function () {
         operation = null
         lastOperand = 0
         inputWindow.value = result
-        inputWindow2.value += '=' + result + ' '
+        addToHistoty('=' + result + ' ');
     }
   
 
@@ -149,4 +148,9 @@ document.getElementById('btn_clr').addEventListener('click', function () {
    
 })
 
-
+function addToHistoty(data){
+    inputWindow2.value += data;
+    while (inputWindow2.value.length > 30) {
+        inputWindow2.value = inputWindow2.value.substring(1)
+    }
+}
